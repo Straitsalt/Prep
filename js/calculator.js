24601a06dfc0fd78cac672954d371a27f26416aa
Straitsalt <pdsawant2010@gmail.com>
2026-07-06 00:02:43 +0530

    Update calculator.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,19 +1,22 @@
 function calculateScore() {
 
-    const correct = parseInt(document.getElementById("correct").value);
-    const incorrect = parseInt(document.getElementById("incorrect").value);
+const correct = Number(document.getElementById("correct").value);
+const incorrect = Number(document.getElementById("incorrect").value);
 
-    if (isNaN(correct) || isNaN(incorrect)) {
-        alert("Please enter both Correct and Incorrect answers.");
-        return;
-    }
+const score = (correct * 4) - incorrect;
 
-    const total = correct + incorrect;
-    const score = (correct * 4) - incorrect;
-    const accuracy = (correct / total) * 100;
+let accuracy = 0;
 
-    document.getElementById("result").innerHTML =
-        "<h2>Score: " + score + "</h2>" +
-        "<h2>Accuracy: " + accuracy.toFixed(2) + "%</h2>" +
-        "<h2>Total Attempted: " + total + "</h2>";
+if (correct + incorrect > 0) {
+    accuracy = (correct / (correct + incorrect)) * 100;
+}
+
+// Save accuracy for Profile page
+localStorage.setItem("accuracy", accuracy.toFixed(2));
+
+document.getElementById("result").innerHTML = `
+<h2>Score : ${score}</h2>
+<h2>Accuracy : ${accuracy.toFixed(2)}%</h2>
+`;
+
 }
