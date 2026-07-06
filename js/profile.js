d92a62c19110c51a6c7b227ff536fb5f256d9f47
Straitsalt <pdsawant2010@gmail.com>
2026-07-06 11:46:55 +0530

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -3,5 +3,8 @@ const history = JSON.parse(localStorage.getItem("dppHistory")) || [];
 document.getElementById("total").innerText = history.length;
 
 const accuracy = localStorage.getItem("accuracy") || 0;
-
 document.getElementById("accuracy").innerText = accuracy + "%";
+
+// Show logged-in user's name
+const currentUser = localStorage.getItem("currentUser") || "Student";
+document.getElementById("name").innerText = currentUser;
