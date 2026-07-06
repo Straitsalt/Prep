ead8d66b1ac5dd8bda720af18751add2cdcb8891
Straitsalt <pdsawant2010@gmail.com>
2026-07-06 12:07:44 +0530

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,18 +1,26 @@
 const currentUser = JSON.parse(localStorage.getItem("currentUser"));
 
-if (currentUser) {
-    document.getElementById("welcome").innerText =
-        `👋 Welcome, ${currentUser.name}!`;
+if(currentUser){
 
-    document.getElementById("subtitle").innerText =
-        `Preparing for ${currentUser.exam}`;
+    document.getElementById("welcome").innerText =
+    "👋 " + currentUser.name;
+
+    const history =
+        JSON.parse(localStorage.getItem(`dppHistory_${currentUser.email}`)) || [];
+
+    document.getElementById("totalDpp").innerText = history.length;
+
+    const accuracy =
+        localStorage.getItem(`accuracy_${currentUser.email}`) || 0;
+
+    document.getElementById("accuracyValue").innerText = accuracy + "%";
 }
 
-function logout() {
+function logout(){
 
     localStorage.removeItem("login");
     localStorage.removeItem("currentUser");
 
-    window.location.href = "index.html";
+    window.location.href="index.html";
 
 }
